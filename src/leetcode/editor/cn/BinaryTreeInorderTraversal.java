f0baa8796d4a1f54576ca850b2b2248df9cba6e7
e <294097569@qq.com>
2023-03-06 21:31:22 +0800

    init

---
+++ src/leetcode/editor/cn/BinaryTreeInorderTraversal.java
@@ -0,0 +1,87 @@
+/**
+ * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：root = []
+ * 输出：[]
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：root = [1]
+ * 输出：[1]
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * <p>
+ * Related Topics 栈 树 深度优先搜索 二叉树 👍 1725 👎 0
+ */
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeInorderTraversal().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            inorder(root, list);
+            return list;
+        }
+
+        private void inorder(TreeNode root, List<Integer> list) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left, list);
+            list.add(root.val);
+            inorder(root.right, list);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
--- src/leetcode/editor/cn/TreeNode.java
+++ src/leetcode/editor/cn/TreeNode.java
@@ -1,15 +1,24 @@
 package leetcode.editor.cn;
 
 /**
- * @description:
- * @author: Ming
+ * Created by ming.
  */
 
 public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x) {
-        val = x;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
